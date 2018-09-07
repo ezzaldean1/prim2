105bad0d43ca372c63d0202687cab532555224e5
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-09-07 13:44:48 +0300

    Create prim2.js

---
+++ prim2.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+
+ 
+
+A7MD.on("guildMemberAdd", member => {
+
+  member.createDM().then(function (channel) {
+
+  return channel.send(**
+
+سلام عليكم 
+
+ممكن تشرفنا بالسيرفر ي بعدي
+
+                               [https://discord.gg/YdrFUeg] 
+                               
+                              
+
+الدعووة خاصة لك ................... [ ${member}  ]
+
+**) 
+
+}).catch(console.error)
+
+})
+
+
+
+A7MD.login(process.env.BOT_TOKEN);
